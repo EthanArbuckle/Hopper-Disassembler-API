65fb2ba3002db5bf25bf214168e5ef10b36b602b
Ethan Arbuckle <ea@sourcetheorem.com>
2021-04-11 18:18:49 -0700

    poc plugin version of proxy

---
+++ hopper_helper/hopper_helper/hopper_helper.h
@@ -0,0 +1,12 @@
+//
+//  hopper_helper.h
+//  hopper_helper
+//
+//  Created by Ethan Arbuckle on 4/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface hopper_helper : NSObject
+
+@end
---
+++ hopper_helper/launcher/launcher.h
@@ -0,0 +1,12 @@
+//
+//  launcher.h
+//  launcher
+//
+//  Created by Ethan Arbuckle on 4/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface launcher : NSObject
+
+@end
